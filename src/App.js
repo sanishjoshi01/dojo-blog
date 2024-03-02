d887a8c27b97e31c4818b1e607f482b468a8f4fa
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-02 12:02:26 +0545

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const person = { name: 'sandesh', age: 23 };
-
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
       <Navbar />
