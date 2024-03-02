7d55dacf6d8e49b3bb22ef0f954a465d0cc109ad
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-02 11:48:33 +0545

    Dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const person = { name: 'sandesh', age: 23 };
+
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
-      <h1>App Component</h1>
+      <h1>{title}</h1>
+      <p>Liked {likes} times</p>
+      <p>{person.age}</p>
+
+      <a href={link}>Go Google</a>
     </div>
   );
 }
