78f14a3f895236d2bd98ce1c8a335f309c350c97
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-05 16:53:36 +0545

    conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,24 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            });
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsLoading(false);
+                });
+        }, 3000)
     }, [])
 
     return (
         <div className="home">
+            {isLoading && <p>Loading...</p>}
             {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
         </div>
     );
