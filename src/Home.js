d8d2dd8c3e561c575f142281776f18625fcc3ab2
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-06 12:29:45 +0545

    handling fetch errrors

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,32 @@ import { useState, useEffect } from "react";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
                     return res.json();
                 })
                 .then(data => {
                     setBlogs(data);
                     setIsLoading(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setIsLoading(false);
                 });
-        }, 3000)
+        }, 1000)
     }, [])
 
     return (
         <div className="home">
+            {error && <p>{error}</p>}
             {isLoading && <p>Loading...</p>}
             {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
         </div>
