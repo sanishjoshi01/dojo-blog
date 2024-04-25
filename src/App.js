d86f25b809aa397ae4ae02a67cf4fed338f3d7f7
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-25 12:24:37 +0545

    exact match route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -8,13 +9,16 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Switch>
-          <div className="content">
-            <Route path='/'>
+        <div className="content">
+          <Switch>
+            <Route exact path='/'>
               <Home />
             </Route>
-          </div>
-        </Switch>
+            <Route path='/create'>
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
---
+++ src/Create.js
@@ -0,0 +1,7 @@
+const Create = () => {
+    return (
+        <h2>Add a new Blog</h2>
+    );
+}
+
+export default Create;
