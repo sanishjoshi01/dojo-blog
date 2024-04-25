5bea6ebcc44a93a0c7eeb41e2d68403712a105bd
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-25 13:30:25 +0545

    submit events

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,16 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author }
+        console.log(blog);
+    }
+
     return (
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
@@ -32,11 +38,6 @@ const Create = () => {
                 </select>
                 <button>Add Blog</button>
             </form>
-
-            <p>{title}</p>
-            <p>{body}</p>
-            <p>{author}</p>
-
         </div>
     );
 }
