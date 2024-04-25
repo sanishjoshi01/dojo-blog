0f6b074374eafe122a8736db9a22db3e4f7539be
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-25 12:43:39 +0545

    useEffect cleanup

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import BlogList from "./BlogList";
-import { useState, useEffect } from "react";
 import useFetch from "./useFetch";
 
 const Home = () => {
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if (!res.ok) {
                         throw Error('could not fetch the data for that resource');
@@ -20,10 +21,17 @@ const useFetch = (url) => {
                     setError(null);
                 })
                 .catch(err => {
-                    setError(err.message);
-                    setIsLoading(false);
+                    if (err.name === "AbortError") {
+                        console.log('fetch aborted');
+                    }
+                    else {
+                        setError(err.message);
+                        setIsLoading(false);
+                    }
                 });
         }, 1000)
+
+        return () => abortCont.abort;
     }, [url])
 
     return { data, isLoading, error }
