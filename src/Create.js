19fb477acdfe267f423ad6f1910658d64a9a5f68
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-26 11:40:59 +0545

    post request to add blogs

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author }
-        console.log(blog);
+
+        setIsLoading(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsLoading(false);
+        })
     }
 
     return (
@@ -36,7 +47,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isLoading && <button>Add Blog</button>}
+                {isLoading && <button disabled>Adding Blog...</button>}
             </form>
         </div>
     );
