00906157e6f15a15f47520c13778f7a155455184
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-26 11:53:55 +0545

    deleting posts

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isLoading } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -14,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
