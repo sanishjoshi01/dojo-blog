e755d8b547754b7619bd524c8df5b01507260068
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-04-26 12:00:08 +0545

    404 pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Create from './Create';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
             </Route>
             <Route path='/blogs/:id'>
               <BlogDetails />
+            </Route>
+            <Route path='*'>
+              <NotFound />
             </Route>
           </Switch>
         </div>
---
+++ src/NotFound.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>Page Not Found</p>
+            <Link to='/'>
+                Back to the homepage...
+            </Link>
+        </div>
+    );
+}
+
+export default NotFound;
